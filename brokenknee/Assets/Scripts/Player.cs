b56c3f532b4ec6e09a31583c9f387eca42e34503
levyofun <58773852+levyofun@users.noreply.github.com>
2021-04-25 16:36:43 +0700

    dima dima dima dima

--- brokenknee/Assets/Scripts/Player.cs
+++ brokenknee/Assets/Scripts/Player.cs
@@ -23,6 +23,6 @@ public class Player : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
-        transform.position += new Vector3(x, y, 0) * Mathf.Pow(speed,-1);
+        transform.position += new Vector3(x, y, 0) * Mathf.Pow(speed, -1);
     }
 }
