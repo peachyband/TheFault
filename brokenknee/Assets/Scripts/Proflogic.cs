4af2568e055d43b4bae8d571c3bff001c0858609
levyofun <58773852+levyofun@users.noreply.github.com>
2021-05-04 23:57:32 +0700

    aaaaa

--- brokenknee/Assets/Scripts/Proflogic.cs
+++ brokenknee/Assets/Scripts/Proflogic.cs
@@ -6,10 +6,19 @@ public class Proflogic : MonoBehaviour
 {
     public Animator anctrl;
     public float cooldownTime;
+    public int currentscene;
+
 
     void Start()
     {
-
+        if(currentscene == 0)
+        {
+            anctrl.Play("Prof_write", 0, 0.0f);
+        }
+        else if(currentscene == 1)
+        {
+            anctrl.Play("Prof_talk", 0, 0.0f);
+        }
     }
 
     public IEnumerator playanim()
